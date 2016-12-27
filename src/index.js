47c47e31152ad4a7d31ca1a87dba545ae29cc0f9
Daniel Andrews <daniel.a.s.andrews@gmail.com>
2016-12-27 14:18:13 -0500

    Added linting via eslint

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import './index.css';
 import numeral from 'numeral';
 
 const value = numeral(1000).format('$0,0.00');
-console.log(`This godly setup is worth ${value} !`);
+console.log(`This godly setup is worth ${value} !`); // eslint-disable-line no-console
