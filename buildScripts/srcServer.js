c75bed524cd3a0681b98a2a4f3b6f04138ccc383
Daniel Andrews <daniel.a.s.andrews@gmail.com>
2017-01-08 20:08:11 -0500

    Still figuring out how to integrate with Coveralls as isparta is deprecated + Added mocking with random sample data for use with mock api via json server and json faker

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,15 @@ app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res){
+	//Hard Coded
+	res.json([
+		{"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com"},
+		{"id": 2, "firstName": "Cindy", "lastName": "Crawford", "email": "cindyc@gmail.com"},
+		{"id": 3, "firstName": "Daniel", "lastName": "Andrews", "email": "daniel.a@gmail.com"}
+	]);
+});
+
 app.listen(port, function(err){
 	if(err){
 		console.log(err);
